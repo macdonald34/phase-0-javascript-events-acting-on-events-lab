5f6d449188612af517e8ed13d425c3bdc0a374ad
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2024-01-04 19:38:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+  function moveDodgerRight() {
+    let left = parseInt(dodger.style.left);
+    if (left < 360) { // 400 (width of game field) - 40 (width of dodger)
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  
