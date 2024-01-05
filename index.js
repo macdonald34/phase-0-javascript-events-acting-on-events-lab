334c039cd400296054de15ee26f37e86f5996218
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2024-01-05 21:09:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 // Your code here
+
+// Your code here
 document.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    } else if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  });
-  
-  function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
-  
-  
-  function moveDodgerRight() {
-    let left = parseInt(dodger.style.left);
-    if (left < 360) { // 400 (width of game field) - 40 (width of dodger)
-      dodger.style.left = `${left + 1}px`;
-    }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-  
-  
+}
+
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left);
+  if (left < 360) { // 400 (width of game field) - 40 (width of dodger)
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
